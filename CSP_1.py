f0f23ce6619bbc317223e666760d4fcc840eb412
vdduong <vdduong@free.fr>
2014-12-04 18:57:05 +0100

    Update CSP_1.py

--- CSP_1.py
+++ CSP_1.py
@@ -1,1 +1,15 @@
 # CSP
+# search a solution by exploring a search tree.
+def solve():
+  if solved():
+    return True
+  i = selectVar()
+  dom = var[i] # save domain
+  for v in dom:
+    if  consistant1(i,v):
+      var[i] = [v]
+      if solve():
+        return True
+  var[i] = dom # .. restore domain
+  return False # backtrack
+
